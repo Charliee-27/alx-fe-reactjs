dcf5fef952dd21629f79b635158e1af68e39d8fd
Charles Mizinga <mizingacharles0@gmail.com>
2024-11-09 23:48:38 +0200

    0. Apply Inline CSS Styling to React Components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer() {
+    return(
+        <footer>
+            <p style={{color:'grey', textAlign:'left'}}><i>© 2024 City Lovers</i></p>
+        </footer>
+    );
+}
+
+export default Footer
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+
+
+function MainContent() {
+    return(
+        <main>
+            <p style={{fontFamily:'courier', backgroundColor:'grey', color:'white', padding:'20px', borderRadius:'20px', margin:'20px'}}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent
