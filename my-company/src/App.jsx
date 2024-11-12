d9c1225a888b7ff0c0fa78a985e9eb4f8559a357
Charles Mizinga <mizingacharles0@gmail.com>
2024-11-12 23:09:01 +0200

    I added the navbar in the App.jsx

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Pagelayout from './components/layout/PageLayout';
+import Navbar from './components/Navbar';
 import Home from './Home';
 import Contact from './Contact';
 import Services from './Services';
@@ -9,6 +10,7 @@ import About from './About';
 function App() {
     return (
         <Router>
+            <Navbar />
             <Routes>
                 <Route path="/" element={<Pagelayout />}>
                     <Route index element={<Home />} />
